1547564ba0325e3ab553dac618390316e26f0d31
zz <zzzsaft@MacBook-Pro.local>
2024-05-30 20:00:34 +0800

    1

--- src/index.ts
+++ src/index.ts
@@ -7,29 +7,27 @@ import cors from "cors";
 import { logger } from "./config/logger";
 import { schedule } from "./schedule";
 import { autoParse } from "./config/autoParse";
-import { testLoginUrl } from "./controllers/xft/login.xft.controller";
 
-// PgDataSource.initialize()
-//   .then(async () => {
-//     logger.info("Data Source has been initialized!");
-//     const app = express();
-//     const port = parseInt(process.env.PORT ?? "2000");
-//     app.use(cors());
-//     app.use(autoParse);
-//     // register all application routes
-//     AppRoutes.forEach((route) => {
-//       app[route.method](
-//         route.path,
-//         (request: Request, response: Response, next: Function) => {
-//           route
-//             .action(request, response)
-//             .then(() => next)
-//             .catch((err) => next(err));
-//         }
-//       );
-//     });
+PgDataSource.initialize()
+  .then(async () => {
+    logger.info("Data Source has been initialized!");
+    const app = express();
+    const port = parseInt(process.env.PORT ?? "2000");
+    app.use(cors());
+    app.use(autoParse);
+    // register all application routes
+    AppRoutes.forEach((route) => {
+      app[route.method](
+        route.path,
+        (request: Request, response: Response, next: Function) => {
+          route
+            .action(request, response)
+            .then(() => next)
+            .catch((err) => next(err));
+        }
+      );
+    });
 
-<<<<<<< HEAD
     // run app
     app.listen(port, () => {
       logger.info(`[server]: Server is running at http://localhost:${port}`);
@@ -45,23 +43,3 @@ process.on("unhandledRejection", (reason, promise) => {
 schedule.forEach((task) => {
   task.start();
 });
-=======
-//     // run app
-//     app.listen(port, () => {
-//       logger.info(`[server]: Server is running at http://localhost:${port}`);
-//     });
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//     logger.error("Error during Data Source initialization:", err);
-//   });
-
-// process.on("unhandledRejection", (reason, promise) => {
-//   logger.error("Unhandled Rejection:", reason);
-// });
-// schedule.forEach((task) => {
-//   task.start();
-// });
-
-console.log(testLoginUrl("LiZhangLi"));
->>>>>>> b80c2fae82ed04d25ef334190967b9e0b9d0cca7
