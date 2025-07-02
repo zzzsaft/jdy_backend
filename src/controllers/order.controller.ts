3ca9d3def103abc0734f1b3a28be61fd6b0376a0
zzzsaft <55004427+zzzsaft@users.noreply.github.com>
2025-07-02 09:54:04 +0800

    filter and format GetOrder items

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -23,6 +23,14 @@ export const getOrderInfo = async (request: Request, response: Response) => {
     if (cus && cus?.name != data.客户名称) {
       data.客户名称 = cus?.name;
     }
+    if (Array.isArray(data.items)) {
+      data.items = data.items
+        .filter((i: any) => i["产品名称"] !== "销售套件")
+        .map((i: any) => ({
+          productCode: i["产品编号"],
+          name: i["产品名称"],
+        }));
+    }
     response.send(data);
   } catch (error) {
     logger.error(error);
